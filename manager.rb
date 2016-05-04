928a600237ceb75c2f1c47edce9fb2572968bc95
jleev <justinviegelahn@gmail.com>
2016-05-04 18:00:16 -0500

    removed gibberish

--- manager.rb
+++ manager.rb
@@ -1,51 +1,49 @@
+# employee1 = ["John", "Smith", 70000, true]
+# employee2 = ["Jane", "doe", 80000, true]
 
-class Transport
+# puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
+# puts "#{employee2[0]} #{employee2[1]} makes #{employee2[-1]} a year."
+# puts
 
-  attr_reader :speed, :direction, :brake
+# employee1 = {first_name: "John", last_name: "Smith", salary: 70000, active: true}
+# employee2 = {:first_name => "Jane", "last_name" => "Doe", "salary" => 80000, "active" => true}
+
+# puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
+# puts "#{employee2['first_name']} #{employee2['last_name']} makes #{employee2['salary']} a year."
+# puts
+
+class Employee
+
+  attr_reader :salary, :active
+  attr_accessor :first_name
 
   def initialize(input_options)
-    @speed = speed
-    @direction = direction
-    @brake = brake
-   end 
-
-   def brake
-    @speed = 0
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    if input_options[:active]
+      @active = input_options[:active]
+    else
+      @active = true
+    end
   end
 
-  def accelerate
-    @speed += 10
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
   end
 
-  def turn(new_direction)
-    @direction = new_direction
-  end 
-
-end
-
-class Car < Transport
-  def initialize(input_options)
-    super(input_options)
-    @speed = 0
-    @direction = 'north'
-  end
-
-  def honk_horn
-    puts "Beeeeeeep!"
-  end
-end
-
-added line 38
-
-class Bike < Transport
-  def initialize(input_options)
-    super(input_options)
-    @speed = 0
-    @direction = 'north'
-  end
-
-  def ring_bell
-    puts "Ring ring!"
+  def give_annual_raise
+    @salary *= 1.05
   end
 end
 
+
+# driver code
+employee1 = Employee.new(
+  last_name: "Smith", 
+  first_name: "John", 
+  salary: 70000, 
+  )
+
+# employee2 = Employee.new({first_name: "Jane", last_name: "Doe", salary: 80000, active: true})
+p employee1.active
